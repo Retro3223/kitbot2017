89414b1bc8d5adaea366d7da3afc37dbc82a200d
Ellery Newcomer <ellery-newcomer@utulsa.edu>
2017-01-21 08:54:18 -0800

    some rotation code

---
+++ src/org/usfirst/frc/team3223/robot/TurningStateMachine.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team3223.robot;
+
+public class TurningStateMachine {
+
+}
---
+++ src/org/usfirst/frc/team3223/robot/TurningStates.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team3223.robot;
+
+public enum TurningStates {
+	Start,
+	Calculate,
+	Drive,
+	End,
+
+}
